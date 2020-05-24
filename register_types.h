13f3c60a710fae9fc0b9700ac9c98cbaa6c60497
Relintai <relintai@gmail.com>
2020-05-24 11:50:55 +0200

    Fix copy paste error.

--- register_types.h
+++ register_types.h
@@ -4,7 +4,6 @@
 /*
 
 Copyright (c) 2020 Péter Magyar
-Copyright(c) 2017-2020 Mattias Edlund
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
